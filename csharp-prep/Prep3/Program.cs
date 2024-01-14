b411a36e90f45dc1e0c354c7e776617ff74b0c98
arkoz84 <arturo-zegarra@hotmail.com>
2024-01-14 00:55:32 -0500

    Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        Console.WriteLine("The computer has selected a magic number between 1 and 100.");
+        Console.WriteLine("What is your guess?");
+        string win = "No";
+        int attempts = 0;
+        int guess = int.Parse(Console.ReadLine());
+        attempts = attempts + 1;
+        while (win == "No")
+        {
+            if (number==guess)
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"You did it in {attempts} attempts.");
+                win = "Yes";
+            }
+            else if (number>guess)
+            {
+                Console.WriteLine("Higher");
+                Console.WriteLine("What is your guess?");
+                guess = int.Parse(Console.ReadLine());
+                attempts = attempts + 1;
+            }
+            else if (number<guess)
+            {
+                Console.WriteLine("Lower");
+                Console.WriteLine("What is your guess?");
+                guess = int.Parse(Console.ReadLine());
+                attempts = attempts + 1;
+            }
+        }
     }
 }
