95b1426dee7c09fc8f5a0b8d0808980d07e845a0
arkoz84 <arturo-zegarra@hotmail.com>
2024-01-14 00:09:45 -0500

    Comments

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,9 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name?");
+        // Ask user for their names
+        Console.WriteLine("What is your first name? ");
         string name = Console.ReadLine();
-        Console.WriteLine("What is your last name?");
+        Console.WriteLine("What is your last name? ");
         string lastname = Console.ReadLine();
         Console.WriteLine("");
         Console.WriteLine("Your name is " + lastname + ", " + name + " " + lastname + ".");
